31e3720311bc8195032d051e2ec58dffd514f2d4
Murmm <weshovskyysky@ro.ru>
2024-07-01 08:02:41 +0700

    Update Ip.sh

--- Ip.sh
+++ Ip.sh
@@ -3,6 +3,18 @@ _ERR=$(grep "command failed" < .ngrok.log)
 if [[ -z "$_ERR" ]]; then
     echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $(jq -r '.inputs.username' $GITHUB_EVENT_PATH)@/" | sed "s/:/ -p /")
 else
-    echo "$_ERR"
-    exit 4
+    if [[ "$_ERR" = *"Your ngrok-agent version"* ]]; then
+        echo "Updating ngrok-agent to the latest version..."
+        ngrok update
+        _ERR=$(grep "command failed" < .ngrok.log)
+        if [[ -z "$_ERR" ]]; then
+            echo $(grep -o -E "tcp://(.+)" < .ngrok.log | sed "s/tcp:\/\//ssh $(jq -r '.inputs.username' $GITHUB_EVENT_PATH)@/" | sed "s/:/ -p /")
+        else
+            echo "$_ERR"
+            exit 4
+        fi
+    else
+        echo "$_ERR"
+        exit 4
+    fi
 fi
